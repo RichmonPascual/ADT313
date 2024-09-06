2f9dc3d04e5bf12422ae5fba1a4aa7d36067a566
Richmon Pascual <pascualrichmon7@gmail.com>
2024-09-06 22:55:06 +0800

    commit

---
+++ ADT313-IT3B/index.php
@@ -0,0 +1,13 @@
+<?php
+   echo "Hello World";
+   $age = 20;
+   $name = "Richmon";
+
+   echo $age;
+   echo $name;
+
+   $x = 10;
+   $y = 50;
+
+   echo $age . "<br><strong>" . $name . "</strong> x+y=" . $x+$y; 
+?>
---
+++ ADT313-IT3B/loop.php
@@ -0,0 +1,7 @@
+<h1>LOOP</h1>
+<?php
+  $colors = array("red","green","blue","yellow");
+  foreach ($colors as $x){
+    echo "$x <br>";
+  }
+?>
